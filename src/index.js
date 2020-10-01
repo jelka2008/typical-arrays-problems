d3dfda95df422b1a4d6b031aee095892075f899d
jelka2008 <jelka2008.rambler.ru>
2020-10-01 16:55:27 +0300

    feat: function min, max and average

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    array.sort((a, b) => {
+        return a - b;
+    });
+    return array[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    array.sort((a, b) => {
+        return b - a;
+    });
+    return array[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((a, b) => a + b) / array.length;
+};
